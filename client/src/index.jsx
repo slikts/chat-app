b2e960b987b5d7cfd090be9da9e659b3a5974163
slikts <dabas@untu.ms>
2019-01-02 15:43:03 +0200

    chore: unused entry

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Global, css } from "@emotion/core";
-import { ApolloProvider } from "react-apollo";
 import App from "./components/App";
-import client from "./client";
 import Wrapper from "./components/Wrapper";
 
 render(
